af56a7da5198a437c6dc232035744c76aeff82be
clopezpro <khrystyan881@hotmail.com>
2023-08-10 09:13:53 -0500

    TEST TAG

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const items = [
   {
     label: 'Activity',
@@ -10,12 +10,21 @@ const items = [
   },
 ];
 const tabActive = ref(0);
-function nextRate() {
-  tabActive.value = 1;
+function nextRate(next: number) {
+  tabActive.value = next;
 }
 </script>
 
 <template>
   TAB ASYNC IS: <span>{{ tabActive }}</span>
   <UTabs v-model="tabActive" :items="items" />
+
+  <div class="flex justify-center">
+    <UButton
+      label="GOT TO TAG 1"
+      color="red"
+      variant="solid"
+      @click="nextRate(1)"
+    />
+  </div>
 </template>
